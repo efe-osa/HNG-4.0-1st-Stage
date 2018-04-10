23afc9177ecc9a587f1bfe4f8a804de4ee7e07a0
jessye95 <32354313+jessye95@users.noreply.github.com>
2018-04-10 12:19:34 +0100

    Add files via upload

---
+++ stage1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Stage One</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="stageOne.css">
+</head>
+
+<body>
+    <div class="containter">
+        <header>Hello World!</header>
+        <p>HNG 4.0 INTERNSHIP</p>
+    </div >
+    <div class="time"><?php
+echo "The time is " . date("h:i:sa");
+?></div>
+<footer>
+    <em>Design by Efe Osawaru Oyelade </em>
+</footer>
+</body>
+</html>
